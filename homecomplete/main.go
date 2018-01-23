2cba5d9ae9b29ba0588597e377e71639cf17667a
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2018-01-23 23:56:50 +0300

    ready autocompleters for OSS and job locations

---
+++ homecomplete/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirkon/complete"
+)
+
+var gopathSrc string
+var Log = complete.Log
+
+func cutParent(path, parent string) string {
+	if !strings.HasPrefix(path, parent) {
+		return path
+	}
+	return path[len(parent)+1:]
+}
+
+type homeProjects string
+
+func (h homeProjects) Predict(args complete.Args) []string {
+	var mask string
+	switch {
+	case args.Last == string(h):
+		mask = filepath.Join(gopathSrc, args.Last, "*")
+	case strings.HasPrefix(args.Last, string(h)):
+		mask = filepath.Join(gopathSrc, args.Last+"*")
+	default:
+		mask = filepath.Join(gopathSrc, string(h), args.Last+"*")
+	}
+	prjs, err := filepath.Glob(mask)
+	if err != nil {
+		Log("cannot get list of files with mask %s: %s", mask, err)
+		os.Exit(1)
+	}
+	for i, path := range prjs {
+		prjs[i] = cutParent(path, gopathSrc)
+	}
+	return prjs
+}
+
+func initGlobals() {
+	usr, err := user.Current()
+	if err != nil {
+		Log("can't get current user: %s", err)
+		os.Exit(1)
+	}
+	gopathSrc = filepath.Join(usr.HomeDir, "Sources", "go", "src")
+}
+
+func main() {
+	initGlobals()
+
+	home := complete.Command{
+		TrustedArgs: homeProjects("github.com/sirkon"),
+	}
+
+	// run the command completion, as part of the main() function.
+	// this triggers the autocompletion when needed.
+	// name must be exactly as the binary that we want to complete.
+	complete.New("home", home).Run()
+}
---
+++ workcomplete/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirkon/complete"
+)
+
+var workDir = "gitlab.stageoffice.ru"
+var gopathSrc string
+var Log = complete.Log
+
+func cutParent(path, parent string) string {
+	if !strings.HasPrefix(path, parent) {
+		return path
+	}
+	return path[len(parent)+1:]
+}
+
+type workProjects string
+
+func (s workProjects) Predict(args complete.Args) (res []string) {
+	defer func() {
+		for i, path := range res {
+			res[i] = cutParent(path, gopathSrc)
+		}
+	}()
+	var root string
+	var dirs []string
+	var err error
+
+	switch {
+	case args.Last == workDir:
+		return complete.PredictDirs(filepath.Join(gopathSrc, args.Last)).Predict(args)
+	case strings.HasPrefix(args.Last, workDir):
+		var dirs []string
+		var err error
+		if stat, err := os.Stat(filepath.Join(gopathSrc, args.Last)); err == nil && stat.IsDir() {
+			dirs, err = filepath.Glob(filepath.Join(gopathSrc, args.Last, "*"))
+		} else {
+			dirs, err = filepath.Glob(filepath.Join(gopathSrc, args.Last+"*"))
+		}
+		if err != nil {
+			Log("cant get list of directories in %workDir: %workDir", root, err)
+			os.Exit(1)
+		}
+		if len(dirs) == 1 && strings.Count(cutParent(dirs[0], gopathSrc), "/") == 1 {
+			Log("matched %s, discovering deeper into it", dirs[0])
+			args.LastCompleted = args.Last
+			args.Last = cutParent(dirs[0], gopathSrc)
+			return s.Predict(args)
+		}
+		return dirs
+	}
+
+	root = filepath.Join(gopathSrc, string(s))
+	dirs, err = filepath.Glob(filepath.Join(root, "*"))
+	if err != nil {
+		Log("cant get list of directories in %workDir: %workDir", root, err)
+		os.Exit(1)
+	}
+	Log("list of dirs: %workDir", dirs)
+	for _, dir := range dirs {
+		mask := filepath.Join(dir, args.Last+"*")
+		files, err := filepath.Glob(mask)
+		if err != nil {
+			Log("cannot get list of files with mask %workDir: %workDir", mask, err)
+			os.Exit(1)
+		}
+		res = append(res, files...)
+	}
+	return res
+}
+
+func initGlobals() {
+	usr, err := user.Current()
+	if err != nil {
+		Log("can't get current user: %workDir", err)
+		os.Exit(1)
+	}
+	gopathSrc = filepath.Join(usr.HomeDir, "Sources", "go", "src")
+}
+
+func main() {
+	initGlobals()
+
+	work := complete.Command{
+		TrustedArgs: workProjects(workDir),
+	}
+	complete.New("work", work).Run()
+}
